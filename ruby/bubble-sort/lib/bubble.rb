7d98b1cf17b7030b590d6823d4a508caed6b6961
cqkh42 <jackcooper93@gmail.com>
2020-09-15 17:02:21 +0100

    fixed bubble sort

--- ruby/bubble-sort/lib/bubble.rb
+++ ruby/bubble-sort/lib/bubble.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-array = [5, 1, 4, 2, 8]
+array = [5, 4, 3, 2, 1]
 iterations = 0
 
 indices = (array.length - 1).downto(1)
 indices.each do |i|
   0.upto i - 1 do |ii|
     iterations += 1
-    array[ii, ii + 1] = [array[ii + 1], array[ii]] if array[ii] > array[ii + 1]
+    array[ii, ii + 2] = array[ii, ii + 2].reverse if array[ii] > array[ii + 1]
   end
 end
 print array
