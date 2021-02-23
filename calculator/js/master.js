3a8461993892495a5bf5694615ad35240c221eee
cqkh42 <jackcooper93@gmail.com>
2021-02-23 15:13:06 +0000

    tidying up

--- calculator/js/master.js
+++ calculator/js/master.js
@@ -1,109 +1,70 @@
-function operate(num1, func, num2) {
-  switch (func) {
-    case "+":
-      return num1 + num2
-      break;
-    case "-":
-      return num1 - num2
-    case "*":
-      return num1 * num2
-    case "/":
-      return num1 / num2
-    default:
-      return 0
+let value = '0'
+let operator = null
+
+function catchNumber(key){
+  const screen = document.querySelector('#screen')
+
+  let newValue = value + key
+  if (newValue == '00'){
+    return
+  } else if (newValue.match(/0\d/)) {
+    value = key
+    screen.innerText = key
+  } else if (!isNaN(newValue)){
+    value += key
+    screen.innerText = newValue
   }
-};
-
-const screen = document.querySelector("#screen")
-const numbers = document.querySelectorAll('button[type="number"]')
-const funcs = document.querySelectorAll('button[type="func"]')
-const equals = document.querySelector('button[type="equals"]')
-const dot = document.querySelector('button[data-num="."]')
-const ac = document.querySelector('button[name="ac"]')
-
-let total = 0;
-let operator = "+";
-let modifier = 0;
-
-let isFirst = true;
-let isNewNumber = true;
-let number = 0;
-let numberArray = []
-
-function reset() {
-  let total = 0;
-  let operator = "+";
-  let modifier = 0;
-
-  let isFirst = true;
-  let isNewNumber = true;
-  let number = 0;
-  let numberArray = []
-  screen.classList.remove("decimal")
-  screen.innerText = number.toLocaleString()
-
-
 }
 
-function numKeyPress(press) {
-  if (press == ".") {
-    screen.classList.add("decimal")
-    dot.disabled = true
-  } else {
-    screen.classList.remove("decimal")
-  }
-  if (isNewNumber) {
-    numberArray = [0, press]
-  } else {
-    numberArray.push(press)
-  }
-  number = Number(numberArray.join(""))
-  screen.innerText = number.toLocaleString();
-  if (isFirst) {
-    total = number
-    modifier = 0;
-  } else {
-    modifier = number
-  }
-  isNewNumber = false
+function catchOperator(key){
+  console.log(key)
+  const operators = {''}
+  operator = key
 }
 
-function operatorPress(press) {
-  dot.disabled = false
-  if (!isFirst) {
-    total = operate(total, operator, modifier)
-    screen.innerText = total.toLocaleString()
+function catchInput(key){
+  console.log(key)
+  if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'].includes(key)) {
+    catchNumber(key)
+  } else if ['*', '/', '+', '-'].includes(key) {
+    catchOperator(key)
   }
-  isFirst = false;
-  operator = press
-  isNewNumber = true
 }
 
-function equalsPress(press) {
-  dot.disabled = false
-  isFirst = true;
-  total = operate(total, operator, modifier)
-  isNewNumber = true;
-  screen.innerText = total.toLocaleString()
+function keyPress(e) {
+  let key = e.key
+  if (key == "Shift") {
+    return
+  } else if (key == "Enter") {
+    key = '='
+  }
+  try {
+    document.querySelector(`button[data-key="${key}"]`).focus()
+    catchInput(key)
+  }
+  catch(err) {
+    return
+  }
 }
 
-document.addEventListener("keyup", event => {
-  if (!isNaN(event.key)) {
-    numKeyPress(event.key)
-  } else if (event.key == ".") {
-    numKeyPress(event.key)
-  } else if ("+-*/".includes(event.key)) {
-    operatorPress(event.key)
-  } else if (event.key == "Enter" || event.key == "=") {
-    equalsPress(event.key)
-  }
-})
+function buttonPress(e) {
+  let key = e.target.dataset.key
+  catchInput(key)
+}
 
-numbers.forEach(item => {
-  item.addEventListener("mouseup", event => numKeyPress(event.target.dataset["num"]))
-});
-funcs.forEach(item => {
-  item.addEventListener("mouseup", event => operatorPress(event.target.dataset["operator"]))
-})
-equals.addEventListener("mouseup", equalsPress)
-ac.addEventListener("mouseup", reset)
+document.querySelectorAll('button').forEach(
+  item => item.addEventListener('mouseup', e=> buttonPress(e))
+)
+
+document.addEventListener(
+  "keydown", e => keyPress(e)
+)
+
+document.addEventListener(
+  "keyup", (e) => document.activeElement.blur()
+)
+
+document.addEventListener(
+  "mouseup", (e) => document.activeElement.blur()
+)
+// document.addEventListener("keydown", (e) => play(e.which))
